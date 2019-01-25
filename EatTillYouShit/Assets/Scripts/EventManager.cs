af7c8cbc04b35696fcb8212bd0ac2f6080ddecef
NVriezen <36536400+NVriezen@users.noreply.github.com>
2019-01-25 21:49:08 +0100

    Unity Setup + EventManager

---
+++ EatTillYouShit/Assets/Scripts/EventManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+//[System.Serializable]
+//public class Event : UnityEvent { }
+
+public class EventManager : MonoBehaviour {
+
+    //private Dictionary<string, Event> eventDictionary;
+    private static EventManager eventManager;
+
+    public static EventManager instance
+    {
+        get
+        {
+            if (!eventManager)
+            {
+                eventManager = FindObjectOfType(typeof(EventManager)) as EventManager;
+                if (!eventManager)
+                {
+                    Debug.LogError("No EventManager found in scene.");
+                }
+            }
+            return eventManager;
+        }
+    }
+
+    //public static void StartListening(string eventName, UnityAction listener){
+    //	Event thisEvent = null;
+    //	if ( instance.eventDictionary.TryGetValue ( eventName, out thisEvent ) ) {
+    //		thisEvent.AddListener ( listener );
+    //	} else {
+    //		thisEvent = new Event();
+    //		thisEvent.AddListener ( listener );
+    //		instance.eventDictionary.Add (eventName, thisEvent);
+    //	}
+    //}
+
+    //public static void StopListening(string eventName, UnityAction listener){
+    //	if ( eventManager == null) {
+    //		return;
+    //	} 
+
+    //	Event thisEvent = null;
+    //	if (instance.eventDictionary.TryGetValue(eventName, out thisEvent)) {
+    //		thisEvent.RemoveListener ( listener );
+    //	}
+    //}
+
+    //public static void TriggerEvent(string eventName){
+    //	Event thisEvent = null;
+    //	if (instance.eventDictionary.TryGetValue(eventName, out thisEvent)){
+    //		thisEvent.Invoke ();
+    //	}
+    //}
+
+    public delegate void OnFinish();
+    public static event OnFinish onFinish;
+
+    public delegate void OnGameStart();
+    public static event OnGameStart onGameStart;
+
+    public static void RaiseOnFinish(float seconds = 0f)
+    {
+        if (onFinish != null)
+        {
+            //instance.Invoke("onFinish", seconds);
+            onFinish();
+            //string name = onFinish.Method.Name;
+            //instance.Invoke(name, seconds) ;
+        }
+    }
+
+    public static void RaiseOnGameStart()
+    {
+        if (onGameStart != null)
+        {
+            onGameStart();
+        }
+    }
+}
---
+++ EatTillYouShit/Assets/Scripts/EventSender.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventSender : MonoBehaviour {
+
+	//public int playerNum = 1;
+ //   private bool holdingCatnip;
+
+ //   void OnTriggerEnter(Collider collider){
+ //       if (collider.tag == "Catnip" && !holdingCatnip) {
+ //           holdingCatnip = true;
+ //           Destroy(collider.gameObject);
+ //       } else if (collider.tag == "ScoreShop" && holdingCatnip)
+ //       {
+ //           holdingCatnip = false;
+ //           Debug.Log("Score +1");
+ //           EventManager.TriggerEvent("SCORE_PLAYER_" + playerNum);
+ //           EventManager.TriggerEvent("SPAWN_CATNIP");
+ //       } else if (collider.tag == "Player")
+ //       {
+ //           //if () //check if doing a dash
+ //           {
+ //               //set int that they collided once with a certain player on both players
+ //               if (collider.gameObject.GetComponent<EventSender>().holdingCatnip)
+ //               {
+ //                   //collider.gameObject.GetComponent<EventSender>().holdingCatnip = false;
+ //                   holdingCatnip = true;
+ //               } else
+ //               {
+ //                   holdingCatnip = false;
+ //               }
+ //           }
+ //       }
+	//}
+}
